2e8933b9118252ba2fade05ba80c42a8e86be12a
Chris Dutra <cdutra@pivotal.io>
2018-03-19 18:24:43 -0700

    WIP 3

--- ci/tasks/windows/test-acceptance-windows-shared.sh
+++ ci/tasks/windows/test-acceptance-windows-shared.sh
@@ -1,28 +1,29 @@
 #!/bin/bash -eux
+main() {
+  source $PWD/bosh-dns-release/ci/assets/utils.sh
 
-set -eu -o pipefail
+  export BBL_STATE_DIR=$PWD/bbl-state/${BBL_STATE_SUBDIRECTORY}
+  source_bbl_env $BBL_STATE_DIR
 
-ROOT_DIR=$PWD
-BBL_STATE_DIR=$ROOT_DIR/envs/$ENV_NAME
+  bosh -n upload-stemcell bosh-candidate-stemcell-windows/*.tgz
+  bosh -n upload-stemcell gcp-linux-stemcell/*.tgz
 
-source $BBL_STATE_DIR/.envrc
+  bosh upload-release candidate-release/*.tgz
 
-bosh -n upload-stemcell $ROOT_DIR/bosh-candidate-stemcell-windows/*.tgz
-bosh -n upload-stemcell $ROOT_DIR/gcp-linux-stemcell/*.tgz
+  bosh -n -d bosh-dns-shared-acceptance deploy bosh-dns-release/src/bosh-dns/test_yml_assets/manifests/shared-acceptance-manifest.yml \
+      --var-file bosh_ca_cert=<(echo "$BOSH_CA_CERT") \
+      -v bosh_client_secret="$BOSH_CLIENT_SECRET" \
+      -v bosh_client="$BOSH_CLIENT" \
+      -v bosh_environment="$BOSH_ENVIRONMENT" \
+      -v base_stemcell=$WINDOWS_OS_VERSION \
+      -v bosh_deployment=bosh-dns \
+      --vars-store dns-creds.yml
 
-bosh upload-release $ROOT_DIR/candidate-release/*.tgz
+  pushd bosh-dns-release/src/bosh-dns/acceptance_tests/dns-acceptance-release
+     bosh create-release --force && bosh upload-release --rebase
+  popd
 
-bosh -n -d bosh-dns-shared-acceptance deploy $ROOT_DIR/bosh-dns-release/src/bosh-dns/test_yml_assets/manifests/shared-acceptance-manifest.yml \
-    --var-file bosh_ca_cert=<(echo "$BOSH_CA_CERT") \
-    -v bosh_client_secret="$BOSH_CLIENT_SECRET" \
-    -v bosh_client="$BOSH_CLIENT" \
-    -v bosh_environment="$BOSH_ENVIRONMENT" \
-    -v base_stemcell=$WINDOWS_OS_VERSION \
-    -v bosh_deployment=bosh-dns \
-    --vars-store dns-creds.yml
+  bosh -d bosh-dns-shared-acceptance run-errand acceptance-tests --keep-alive
+}
 
-pushd $ROOT_DIR/bosh-dns-release/src/bosh-dns/acceptance_tests/dns-acceptance-release
-   bosh create-release --force && bosh upload-release --rebase
-popd
-
-bosh -d bosh-dns-shared-acceptance run-errand acceptance-tests --keep-alive
+main
--- ci/tasks/windows/test-acceptance-windows.sh
+++ ci/tasks/windows/test-acceptance-windows.sh
@@ -1,22 +1,21 @@
 #!/bin/bash -eux
+main() {
+  source $PWD/bosh-dns-release/ci/assets/utils.sh
 
-set -eu -o pipefail
+  export BBL_STATE_DIR=$PWD/bbl-state/${BBL_STATE_SUBDIRECTORY}
+  source_bbl_env $BBL_STATE_DIR
 
-ROOT_DIR=$PWD
-BBL_STATE_DIR=$ROOT_DIR/envs/$ENV_NAME
+  bosh -n upload-stemcell bosh-candidate-stemcell-windows/*.tgz
 
-source $BBL_STATE_DIR/.envrc
+  export BOSH_DEPLOYMENT=bosh-dns-windows-acceptance
 
-bosh -n upload-stemcell $ROOT_DIR/bosh-candidate-stemcell-windows/*.tgz
+  bosh upload-release candidate-release/*.tgz
 
-export BOSH_DEPLOYMENT=bosh-dns-windows-acceptance
+  bosh -n deploy bosh-dns-release/src/bosh-dns/test_yml_assets/manifests/windows-acceptance-manifest.yml \
+    -v health_server_port=2345 \
+    -v windows_stemcell=$WINDOWS_OS_VERSION \
+    -o bosh-dns-release/src/bosh-dns/test_yml_assets/ops/enable-health-manifest-ops.yml \
+    --vars-store dns-creds.yml
 
-bosh upload-release $ROOT_DIR/candidate-release/*.tgz
-
-bosh -n deploy $ROOT_DIR/bosh-dns-release/src/bosh-dns/test_yml_assets/manifests/windows-acceptance-manifest.yml \
-  -v health_server_port=2345 \
-  -v windows_stemcell=$WINDOWS_OS_VERSION \
-  -o $ROOT_DIR/bosh-dns-release/src/bosh-dns/test_yml_assets/ops/enable-health-manifest-ops.yml \
-  --vars-store dns-creds.yml
-
-bosh run-errand acceptance-tests-windows
+  bosh run-errand acceptance-tests-windows
+}
