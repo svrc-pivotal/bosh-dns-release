7e122ca15f15d9ee6aa1247aa451b0e2a5e911f3
Danny Berger <dberger@pivotal.io>
2018-03-21 09:36:27 -0700

    clean up director

--- ci/tasks/windows/destroy-env.sh
+++ ci/tasks/windows/destroy-env.sh
@@ -5,6 +5,11 @@ main() {
   local bbl_state_env_repo_dir=$PWD/bbl-state
   trap "commit_bbl_state_dir ${bbl_state_env_repo_dir} ${BBL_STATE_DIR} ${output_dir} 'Remove bbl state dir'" EXIT
 
+  (
+    source_bbl_env bbl-state/${BBL_STATE_DIR}
+    clean_up_director
+  )
+
   pushd bbl-state/${BBL_STATE_DIR}
     bbl version
     bbl --debug destroy --no-confirm
