512bf7b7828fff4f984fe14e1ce720183ed91f5e
Chris Dutra <cdutra@pivotal.io>
2018-03-20 19:24:27 -0700

    fix things

--- ci/tasks/windows/test-acceptance-windows-nameserver-disabled.sh
+++ ci/tasks/windows/test-acceptance-windows-nameserver-disabled.sh
@@ -14,7 +14,7 @@ main() {
       bosh-dns-release/src/bosh-dns/test_yml_assets/manifests/windows-acceptance-manifest.yml \
       -o bosh-dns-release/src/bosh-dns/acceptance_tests/windows/disable_nameserver_override/manifest-ops.yml \
       -v windows_stemcell=$WINDOWS_OS_VERSION \
-      -v deployment_name=bosh-dns-windows-acceptance \
+      -v deployment_name=bosh-dns-windows-nameserver-disable-acceptance \
       --vars-store dns-creds.yml
 
   bosh run-errand acceptance-tests-windows --keep-alive
